5b392e8316c9e5ebca314c09512ff5330b46174e
Seasoned Apprentice <7marvit@gmail.com>
2023-01-03 17:30:46 +0000

    fiilled migration

--- migrations/Version20230103172116.php
+++ migrations/Version20230103172116.php
@@ -20,7 +20,15 @@ final class Version20230103172116 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('
+            CREATE TABLE sessions (
+                sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+                sess_data BYTEA NOT NULL,
+                sess_lifetime INTEGER NOT NULL,
+                sess_time INTEGER NOT NULL
+            )
+        ');
+        $this->addSql('CREATE INDEX expiry ON sessions (sess_lifetime)');
     }
 
     public function down(Schema $schema): void
