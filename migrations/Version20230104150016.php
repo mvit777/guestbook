6b85e409adfe59a25a155d425749eb718283bb61
Seasoned Apprentice <7marvit@gmail.com>
2023-01-04 15:06:22 +0000

    fixing migration

--- migrations/Version20230104150016.php
+++ migrations/Version20230104150016.php
@@ -20,7 +20,9 @@ final class Version20230104150016 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE conference ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE conference ADD slug VARCHAR(255)');
+        $this->addSql("UPDATE conference SET slug=CONCAT(LOWER(city), '-', year)");
+        $this->addSql('ALTER TABLE conference ALTER COLUMN slug SET NOT NULL');
     }
 
     public function down(Schema $schema): void
